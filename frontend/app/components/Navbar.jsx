ff076d1816b262f42f36e2ac08eb4de4d4062df3
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2025-10-29 18:55:57 +0530

    create a Navbar and put the all menu-bar content but there are some functionallity give some errors

--- frontend/app/components/Navbar.jsx
+++ frontend/app/components/Navbar.jsx
@@ -1,24 +1,36 @@
 "use client";
+import "../SytleFiles/Navbar.css";
 import MyImage from 'next/image';
 import {useState } from "react";
 import Link from 'next/link';
 // import link from 'next/Link';
 
 export default function Navbar() {
-    const [count, setcount] = useState(0);
-    const handleclick= () => {
-        setcount(count + 1);
-    }
+    // const [CheckStatus, ChangeStatus] = useState("true");
+    // const toggleForm= (Check) => {
+    //     if (CheckStatus === "true") {
+    //         ChangeStatus("false");
+    //         console.log("Clicked and change  by to false");
+    //     }        else {
+    //         ChangeStatus("true");
+    //         console.log("Clicked and change by to true");
+    //     }
+    // }
+
     return (
         <>
             <header className="navbar-container-area">
                 <div className="navbar-container">
                     <nav className="navbar-inner-container">
                         <div className="navbar-logo-container">
-                            <img src={MyImage} alt="" />
+                            <img src="../Assets/NuserIcon.jpg" alt="Logo" />
                             <h2>Nikhil Sharma</h2>
                         </div>
                         <div className="navbar-content-container">
+                            <div className="change-toggle-status-button-container">
+                                <button className="three-line-bar-icon" > 3Line</button>
+                                <button className="cross-icon" >Cross</button>
+                            </div>
                             <div className="navbar-inner-content">
                                 <button className=""><Link href="/">Home</Link></button>
                                 <button className=""><Link href="/About">About</Link></button>
