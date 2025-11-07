61461372de0e27aeed1a9bdd7096d739ffe2b5a4
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2025-11-07 15:32:19 +0530

    Create a Projects page

--- frontend/app/components/Projects.jsx
+++ frontend/app/components/Projects.jsx
@@ -1,24 +1,24 @@
 "use client";
 
 import "../SytleFiles/Projects.css";
+import Image from "next/image";
+import projectImage from "../Assets/desktop.png";
 
 const projects = [
   {
     id: 1,
     title: "Portfolio Website",
     tech: "Personal Portfolio",
-    description:
-      "A personal portfolio website to showcase my projects and skills.",
-    imageUrl: "/images/portfolio.png",
+    description:"A personal portfolio website to showcase my projects and skills.",
+    imageUrl: projectImage,
     projectUrl: "https://myportfolio.com",
   },
   {
     id: 2,
     title: "Todo App",
     tech: "React + Node.js",
-    description:
-      "A simple task management app with authentication and API integration.",
-    imageUrl: "/images/todo.png",
+    description:"A simple task management app with authentication and API integration.",
+    imageUrl: projectImage,
     projectUrl: "https://mytodoapp.com",
   },
 ];
@@ -37,7 +37,21 @@ export default function Projects() {
         </div>
 
         <div className="project-list">
-        
+          {projects.map((project) => (
+            <div key = {project.id} className="project-card" >
+              <div className="project-view-card-area">
+                <img src={project.imageUrl}
+                  alt={project.title} />
+                </div>
+              <div className="project-detail-content">
+                <h2 className="project-title">{project.title}</h2>
+                <p className="project-tech">{project.tech}</p>
+                <p className="project-description">{project.description}</p>
+                  <div className=""><button className="">Case Study</button></div>
+                  <div className=""><button className="">Live View</button></div>
+              </div>
+             </div>
+          ))}
         </div>
       </div>
     </section>
